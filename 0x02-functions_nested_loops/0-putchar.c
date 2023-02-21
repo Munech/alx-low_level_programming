91303df06ced79f273d76c68170a9405b0f1b91b
Munech <collomunec@gmail.com>
2023-02-21 19:15:24 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-int main(void)
-{
- {
- putchar("%d \n", "_putchar");
-}
-return (0);
-}
 
+int main() {
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+    return 0;
+}
