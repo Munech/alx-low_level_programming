f5e62458fc7ac418401dbb2592f6839c7e95de34
Munech <collomunec@gmail.com>
2023-02-23 20:34:36 +0300

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
 int i;
 
-printf("Infinite loop incoming :(\n");
+puts("Infinite loop incoming :(\n");
 
 i = 0;
 /*
@@ -18,7 +18,7 @@ i = 0;
 *putchar(i);
 *}
 */
-printf("Infinite loop avoided! \\o/\n");
+puts("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
